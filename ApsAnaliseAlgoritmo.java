b605f6760648ada2fcfea4a1494daea870b9beb0
vifelisberto <vi.felis@hotmail.com>
2020-05-11 21:48:51 -0300

    Pior e melhor caso

--- ApsAnaliseAlgoritmo.java
+++ ApsAnaliseAlgoritmo.java
@@ -1,31 +1,48 @@
-import java.util.Arrays;
-import java.util.Random;
-
 public class ApsAnaliseAlgoritmo {
     public static void main(final String[] args) throws Exception {
-
-        int[] vetor = new int[20000];
-
-        Random random = new Random();
-
-        for (int i = 0; i < 20000; i++) {
-            int numero = random.nextInt(20000);
-            vetor[i] = numero;
-        }
-
-        // System.out.println("Não Ordenado: ");
-        // System.out.println(Arrays.toString(vetor));
-
-        long tempoInicio = System.currentTimeMillis();
+        int[] vetorPiorCaso = gerarVetorPiorCaso();
+        int[] vetorMelhorCaso = gerarVetorMelhorCaso();
 
         final HeapSort heap = new HeapSort();
-        heap.sort(vetor);
 
-        long tempo = (System.currentTimeMillis() - tempoInicio);
+        // Pior caso
+        long tempoInicio = System.currentTimeMillis();
 
-        // System.out.println("Ordenado: ");
-        // System.out.println(Arrays.toString(vetor));
+        heap.sort(vetorPiorCaso);
 
-        System.out.println("Tempo Total: " + tempo + " segundos");
+        long tempoPiorCaso = (System.currentTimeMillis() - tempoInicio);
+
+        System.out.println("\n################ Tempo Total Pior caso: " + tempoPiorCaso + " ################");
+
+        // Melhor Caso
+        tempoInicio = System.currentTimeMillis();
+
+        heap.sort(vetorMelhorCaso);
+
+        long tempoMelhorCaso = (System.currentTimeMillis() - tempoInicio);
+
+        System.out.println("\n################ Tempo Total Melhor caso: " + tempoMelhorCaso + " ################");
+    }
+
+    private static int[] gerarVetorPiorCaso() {
+        int QTD_ITEMS = 20000;
+        int[] vetor = new int[QTD_ITEMS];
+
+        for (int i = 0; i < QTD_ITEMS; i++) {
+            vetor[i] = QTD_ITEMS - i;
+        }
+
+        return vetor;
+    }
+
+    private static int[] gerarVetorMelhorCaso() {
+        int QTD_ITEMS = 20000;
+        int[] vetor = new int[QTD_ITEMS];
+
+        for (int i = 0; i < QTD_ITEMS; i++) {
+            vetor[i] = i + 1;
+        }
+
+        return vetor;
     }
 }
